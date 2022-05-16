824032da8fb9fe3e6c95f0b38bad9a838fe16711
KODY LOW <kody@seas.upenn.edu>
2022-05-16 00:43:54 +0000

    fixed decrypt error

--- caesar.py
+++ caesar.py
@@ -5,15 +5,16 @@ def encrypt(key,plaintext):
     #assume only uppercase letters, can just use ord("A")
     for letter in plaintext:
         ciphertext += chr((ord(letter) - ord("A") + key) % 26 + ord("A"))
-
+    print(ciphertext)
     return ciphertext
 
 def decrypt(key,ciphertext):
-    plaintext=ciphertext
+    plaintext=""
     #YOUR CODE HERE
     #assume only uppercase letters, can just use ord("A")
     for letter in ciphertext:
         plaintext += chr((ord(letter) - ord("A") - key) % 26 + ord("A"))
+    print(plaintext)
     return plaintext
 
 
